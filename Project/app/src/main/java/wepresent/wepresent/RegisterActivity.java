2e02ba3d9a2ec7fcb6506b838f2c256c9ae647e7
TheLaurens <Laurens.Hagendoorn@gmail.com>
2015-03-17 10:24:06 +0100

    it says I changed things

--- Project/app/src/main/java/wepresent/wepresent/RegisterActivity.java
+++ Project/app/src/main/java/wepresent/wepresent/RegisterActivity.java
@@ -1,12 +1,13 @@
 package wepresent.wepresent;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
