9b18379d1360b88491bc45967671a40870987a7c
Chris Jansen <chris223j@hotmail.com>
2015-03-23 13:51:58 +0100

    Overbodige shit verwijderd
    Zie title.

--- Project/app/src/main/java/wepresent/wepresent/MainActivity.java
+++ Project/app/src/main/java/wepresent/wepresent/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends Activity implements AsyncTaskReport {
     private MainMapper loginMapper;
     private EditText input_username;
     private EditText input_password;
-    private String androidId;
     private Button loginButton;
 
     // Google Cloud Messaging
@@ -52,10 +51,10 @@ public class MainActivity extends Activity implements AsyncTaskReport {
         // Google Cloud Messaging
         pushClientManager = new GCMClientManager(this, PROJECT_NUMBER);
         pushClientManager.registerIfNeeded(new GCMClientManager.RegistrationCompletedHandler() {
-           @Override
+            @Override
             public void onSuccess(String registrationId, boolean isNewRegistration) {
                uniqueDeviceId = registrationId;
-           }
+            }
 
             @Override
             public void onFailure(String ex) {
@@ -66,7 +65,6 @@ public class MainActivity extends Activity implements AsyncTaskReport {
 
         input_username = (EditText) findViewById(R.id.userName);
         input_password = (EditText) findViewById(R.id.passWord);
-        androidId = getUniquePsuedoID();
 
         loginButton = (Button) findViewById(R.id.login);
 
@@ -123,43 +121,5 @@ public class MainActivity extends Activity implements AsyncTaskReport {
         intent.putExtra("AndroidID", uniqueDeviceId);
         startActivity(intent);
     }
-
-    /**
-     * Return pseudo unique ID
-     * @return ID
-     */
-    public static String getUniquePsuedoID() {
-        // If all else fails, if the user does have lower than API 9 (lower
-        // than Gingerbread), has reset their device or 'Secure.ANDROID_ID'
-        // returns 'null', then simply the ID returned will be solely based
-        // off their Android device information. This is where the collisions
-        // can happen.
-        // Thanks http://www.pocketmagic.net/?p=1662!
-        // Try not to use DISPLAY, HOST or ID - these items could change.
-        // If there are collisions, there will be overlapping data
-        String m_szDevIDShort = "35" + (Build.BOARD.length() % 10) + (Build.BRAND.length() % 10) + (Build.CPU_ABI.length() % 10) + (Build.DEVICE.length() % 10) + (Build.MANUFACTURER.length() % 10) + (Build.MODEL.length() % 10) + (Build.PRODUCT.length() % 10);
-
-        // Thanks to @Roman SL!
-        // http://stackoverflow.com/a/4789483/950427
-        // Only devices with API >= 9 have android.os.Build.SERIAL
-        // http://developer.android.com/reference/android/os/Build.html#SERIAL
-        // If a user upgrades software or roots their device, there will be a duplicate entry
-        String serial = null;
-        try {
-            serial = android.os.Build.class.getField("SERIAL").get(null).toString();
-
-            // Go ahead and return the serial for api => 9
-            return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
-        } catch (Exception exception) {
-            // String needs to be initialized
-            serial = "serial"; // some value
-        }
-
-        // Thanks @Joe!
-        // http://stackoverflow.com/a/2853253/950427
-        // Finally, combine the values we have found by using the UUID class to create a unique identifier
-        return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
-    }
-
 
 }
